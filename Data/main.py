cd08a0b35561dbf9a79c410461577d067a575627
Ahkfor <90811508+Ahkfor@users.noreply.github.com>
2024-10-09 20:51:33 -0400

    Delete Data directory

--- Data/main.py
+++
@@ -1,9 +0,0 @@
-import visualization
-from HAPI.hapi_class import HapiClass
-
-
-# Example
-country = "AFG"
-country_data = HapiClass(country)
-# visualization.plot_humanitarian_needs(country_data)
-visualization.plot_conflict_events(country_data)
